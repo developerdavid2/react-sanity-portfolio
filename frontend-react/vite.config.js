30cb3053495c27e5a0381f1ce42afce94f63fbcc
developerdavid2 <jacobsdavid.dr@gmail.com>
2025-01-30 02:33:39 +0100

    Fixed Vite dist issue

--- frontend-react/vite.config.js
+++ frontend-react/vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Ensure build output goes into "dist"
+    outDir: "dist", // Ensures Vite outputs to 'dist'
   },
 });
